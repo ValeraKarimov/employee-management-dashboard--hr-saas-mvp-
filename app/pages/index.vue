26172127cc3450b628c45cefe8406ab6a51b69d9
Valerij <valeraguess@gmail.com>
2026-04-24 11:31:21 +0200

    fix: add root redirect to login

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useAuthStore } from '~/stores/auth'
+
+const auth = useAuthStore()
+
+if (auth.user) {
+  navigateTo('/dashboard')
+} else {
+  navigateTo('/login')
+}
+</script>
